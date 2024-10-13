2613033ab725b5c133cf6fa87e105aad50bca396
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-10-13 10:14:42 +0530

    Conflict Resolve.

--- js/roundSelection.js
+++ js/roundSelection.js
@@ -7,13 +7,13 @@ window.addEventListener("load", function () {
     let progress = 0;
     const interval = setInterval(function() {
         if (progress >= 100) {
-            clearInterval(interval);  
-            loadingScreen.style.display = "none";  
-            content.style.display = "block";  
+            clearInterval(interval);
+            loadingScreen.style.display = "none";
+            content.style.display = "block";
         } else {
-            progress += 1; 
-            loadingBar.style.width = progress + "%"; 
-            loadingText.innerText = progress + "%";  
+            progress += 1;
+            loadingBar.style.width = progress + "%";
+            loadingText.innerText = progress + "%";
         }
-    }, 30);  
+    }, 30);
 });
